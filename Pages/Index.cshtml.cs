9b61e18d0be59e71857cdc39b5d8679c38757e01
TheJP <jp_55555@hotmail.com>
2018-11-14 21:52:44 +0100

    Started to work on view cards

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MightyHomeAutomation.Persistence;
 
 namespace MightyHomeAutomation.Pages
 {
     public class IndexModel : PageModel
     {
+        public Configuration Configuration { get; }
+
+        public IndexModel(Configuration configuration) => Configuration = configuration;
+
         public void OnGet()
         {
 
